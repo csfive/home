6ff0de96a39e8005cd77604e31f028405bf2e6df
mancuoj <1337381768@qq.com>
2023-01-02 03:36:22 +0800

    fix: giscus refresh

--- docs/.vitepress/theme/components/Comment.vue
+++ docs/.vitepress/theme/components/Comment.vue
@@ -1,24 +1,18 @@
 <script setup>
 import Giscus from '@giscus/vue'
-import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 import { computed } from 'vue'
 
 const { isDark } = useData()
-const { Layout } = DefaultTheme
 const theme = computed(() => isDark.value ? 'transparent_dark' : 'light')
 </script>
 
 <template>
-  <Layout>
-    <template #doc-after>
-      <div id="giscus">
-        <Giscus id="comments" repo="csfive/docs" repoId="R_kgDOIr671A" category="Announcements"
-          categoryId="DIC_kwDOIr671M4CTWpS" mapping="pathname" strict="1" reactionsEnabled="1" emitMetadata="0"
-          inputPosition="top" lang="zh-CN" loading="lazy" :theme="theme" />
-      </div>
-    </template>
-  </Layout>
+  <div id="giscus">
+    <Giscus id="comments" repo="csfive/docs" repoId="R_kgDOIr671A" category="Announcements"
+      categoryId="DIC_kwDOIr671M4CTWpS" mapping="pathname" strict="1" reactionsEnabled="1" emitMetadata="0"
+      inputPosition="top" lang="zh-CN" loading="lazy" :theme="theme" />
+  </div>
 </template>
 
 <style scoped>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,27 @@
+import { h, watch } from 'vue'
+import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Comment from './components/Comment.vue'
 import './custom.css'
 
 export default {
     ...DefaultTheme,
-    Layout: Comment
+    Layout() {
+        return h(DefaultTheme.Layout, null, {
+            'doc-after': () => h(Comment)
+        })
+    },
+    setup() {
+        const route = useRoute()
+        watch(
+            () => route.path,
+            () => {
+                const pathname = localStorage.getItem('pathname')
+                if (pathname !== route.path) {
+                    localStorage.setItem('pathname', route.path)
+                    location.reload()
+                }
+            }
+        )
+    }
 }
