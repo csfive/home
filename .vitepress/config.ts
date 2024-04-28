9804644713d9d026c844cee8695b11dca9685588
mancuoj <mancuoj@gmail.com>
2024-04-29 00:27:48 +0800

    chore: update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,5 @@
 import { DefaultTheme, defineConfig } from 'vitepress'
+import { withMermaid } from 'vitepress-plugin-mermaid'
 
 const CS61A: DefaultTheme.NavItemWithLink[] = [{ text: 'Guide', link: '/cs61a/guide' }]
 
@@ -19,51 +20,53 @@ const PREFERENCES: DefaultTheme.NavItemWithLink[] = [
   { text: 'Proxy', link: '/refs/proxy' },
 ]
 
-export default defineConfig({
-  title: 'CS Five',
-  titleTemplate: ':title',
-  description: '计算机废物自学指北',
-  srcDir: 'docs',
-  cleanUrls: true,
-  lang: 'zh-CN',
-  head: [['link', { rel: 'icon', href: '/logo.svg' }]],
-  vite: {
-    ssr: {
-      noExternal: ['@nolebase/vitepress-plugin-enhanced-readabilities'],
+export default withMermaid(
+  defineConfig({
+    title: 'CS Five',
+    titleTemplate: ':title',
+    description: '计算机废物自学指北',
+    srcDir: 'docs',
+    cleanUrls: true,
+    lang: 'zh-CN',
+    head: [['link', { rel: 'icon', href: '/logo.svg' }]],
+    vite: {
+      ssr: {
+        noExternal: ['@nolebase/vitepress-plugin-enhanced-readabilities'],
+      },
     },
-  },
-  markdown: {
-    math: true,
-    image: {
-      lazyLoading: true,
+    markdown: {
+      math: true,
+      image: {
+        lazyLoading: true,
+      },
     },
-  },
-  themeConfig: {
-    logo: { src: '/logo.svg' },
-    nav: [
-      {
-        text: 'CS',
-        items: [
-          { text: 'CS61A', items: CS61A },
-          { text: 'CS61B', items: CS61B },
+    themeConfig: {
+      logo: { src: '/logo.svg' },
+      nav: [
+        {
+          text: 'CS',
+          items: [
+            { text: 'CS61A', items: CS61A },
+            { text: 'CS61B', items: CS61B },
+          ],
+        },
+        { text: 'Python', items: PYTHON },
+        { text: 'Preferences', items: PREFERENCES },
+      ],
+      sidebar: {
+        '/': [
+          { text: 'CS61A', collapsed: false, items: CS61A },
+          { text: 'Python', collapsed: false, items: PYTHON },
+          { text: 'CS61B', collapsed: true, items: CS61B },
+          { text: 'References', collapsed: false, items: PREFERENCES },
         ],
       },
-      { text: 'Python', items: PYTHON },
-      { text: 'Preferences', items: PREFERENCES },
-    ],
-    sidebar: {
-      '/': [
-        { text: 'CS61A', collapsed: false, items: CS61A },
-        { text: 'Python', collapsed: false, items: PYTHON },
-        { text: 'CS61B', collapsed: true, items: CS61B },
-        { text: 'References', collapsed: false, items: PREFERENCES },
-      ],
+      socialLinks: [{ icon: 'github', link: 'https://github.com/csfive' }],
+      search: { provider: 'local' },
+      lastUpdated: {
+        text: 'Last Updated',
+        formatOptions: { dateStyle: 'short', timeStyle: 'short' },
+      },
     },
-    socialLinks: [{ icon: 'github', link: 'https://github.com/csfive' }],
-    search: { provider: 'local' },
-    lastUpdated: {
-      text: 'Last Updated',
-      formatOptions: { dateStyle: 'short', timeStyle: 'short' },
-    },
-  },
-})
+  }),
+)
--- docs/py/syntax.md
+++ docs/py/syntax.md
@@ -1,10 +1,45 @@
 # Syntax Basics
 
 - [The Python Tutorial](https://docs.python.org/3/tutorial/index.html)，官方教程
+- [Python Crash Course](https://book.douban.com/subject/36365320/)，Python 编程从入门到实战
 - [Comprehensive Python Cheatsheet](https://gto76.github.io/python-cheatsheet/)
 
-## Guess the Number Game
+## Tic-Tac-Toe
 
-从一个简单的游戏入手，来熟悉 Python 语法。
+> 本文目标读者是那些学了很多语法但不知道下一步该做什么的人
 
-TODO
+井字棋，大家应该都玩过，首先我们需要一个长宽相同的 3 ✖️ 3 的棋盘：
+
+```py
+BOARD_SIZE = 3
+```
+
+棋盘可以用一个简单的嵌套列表来表示，每个元素就代表一个棋盘格，棋盘格内有三种状态：`X`, `O` 和 `None` 也就是空：
+
+```py
+[
+  ['X', None, 'O'],
+  ['O', 'X', None],
+  [None, None, 'X']
+]
+```
+
+那么第一个任务就是 `new_board()` 函数，返回一个空棋盘也就是一个 3 ✖️ 3 的全为 `None` 的嵌套列表：
+
+```py
+def new_board():
+    board = []
+    for _ in range(BOARD_SIZE):
+        col = []
+        for _ in range(BOARD_SIZE):
+            col.append(None)
+        col.append(None)
+    return board
+```
+
+可以使用 [列表推导式](https://docs.python.org/zh-cn/3/tutorial/datastructures.html#list-comprehensions) 简化以上代码：
+
+```py
+def new_board():
+    return [[None for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+```
